b868bcf11197bba7758032b47e542ab024cfbc5f
Richard <rinaldy.richard.03@gmail.com>
2022-04-05 20:33:12 +0700

    fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,15 @@
-import { ImageProps } from '@chakra-ui/react'
+import { ImageProps, Image } from '@chakra-ui/react'
 
 const Logo = (rest: ImageProps) => {
-  return <Logo />
+  return (
+    <Image
+      height="40px"
+      objectFit="cover"
+      src="https://www.nicepng.com/png/full/1-14636_pokemon-logo-text-png-7-pokemon-gotta-catch.png"
+      alt="Pokemon"
+      fallbackSrc="https://via.placeholder.com/100"
+    />
+  )
 }
 
 export default Logo
