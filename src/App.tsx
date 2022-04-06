cc88c7994f3571cde818697f77dc0b99d4ce797b
Richard <rinaldy.richard.03@gmail.com>
2022-04-06 13:09:06 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import NavigatorWrapper from './components/navigation/NavigatorWrapper'
+import PokemonDetail from './components/views/PokemonDetail'
 import PokemonList from './components/views/PokemonList'
 
 export const App = () => {
@@ -8,6 +9,9 @@ export const App = () => {
       <NavigatorWrapper>
         <Routes>
           <Route path="/" element={<PokemonList />} />
+          <Route path="pokemon">
+            <Route path=":pokemonId" element={<PokemonDetail />} />
+          </Route>
         </Routes>
       </NavigatorWrapper>
     </>
