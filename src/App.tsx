d2f1c3989fe735815154c8564ea6d20ecaf1a602
Richard <rinaldy.richard.03@gmail.com>
2022-04-06 14:32:37 +0700

    delete unused route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import NavigatorWrapper from './components/navigation/NavigatorWrapper'
-import PokemonDetail from './components/views/PokemonDetail'
 import PokemonList from './components/views/PokemonList'
 
 export const App = () => {
@@ -9,9 +8,6 @@ export const App = () => {
       <NavigatorWrapper>
         <Routes>
           <Route path="/" element={<PokemonList />} />
-          <Route path="pokemon">
-            <Route path=":pokemonId" element={<PokemonDetail />} />
-          </Route>
         </Routes>
       </NavigatorWrapper>
     </>
