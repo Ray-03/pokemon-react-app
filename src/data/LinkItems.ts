3b932757d7fdf4bc6be269cc281f16f834ae4c96
Richard <rinaldy.richard.03@gmail.com>
2022-04-06 17:34:40 +0700

    add url sidebar

--- src/data/LinkItems.ts
+++ src/data/LinkItems.ts
@@ -1,5 +1,8 @@
 import { LinkItemProps } from './interfaces'
 
-const LinkItems: Array<LinkItemProps> = [{ name: 'Home', url: '/' }]
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'Home', url: '/' },
+  { name: 'My Pokemon', url: '/my-pokemon' },
+]
 
 export { LinkItems }
