fb6e4d20dc823e0abe14483924a635e92e89f5fd
Richard <rinaldy.richard.03@gmail.com>
2022-04-06 12:46:24 +0700

    create interfaces

--- src/components/views/PokemonList.tsx
+++ src/components/views/PokemonList.tsx
@@ -1,4 +1,25 @@
-import React from 'react'
+import { Box, Center, Heading, Spinner, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { useQuery } from 'react-query'
+import mainApiUrl from '../../data/api'
+
+interface RequestDataType {
+  page: number
+  limit: number
+}
+
+interface PokemonListProps {
+  count: number
+  next: string
+  previous: string
+  results: Array<PokemonNameUrlProps>
+}
+
+interface PokemonNameUrlProps {
+  name: string
+  url: string
+}
 
 const PokemonList = () => {
   return <div>PokemonList</div>
