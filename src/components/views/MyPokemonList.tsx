327634e1fb4f4987193f4de82f10164b30559940
Richard <rinaldy.richard.03@gmail.com>
2022-04-06 17:36:28 +0700

    create my pokemon list page

---
+++ src/components/views/MyPokemonList.tsx
@@ -0,0 +1,8 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const MyPokemonList = () => {
+  return <Heading>My Pokemon List</Heading>
+}
+
+export default MyPokemonList
